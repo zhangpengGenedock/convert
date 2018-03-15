593e83a03d9c8f14fef04f623370313792b6f960
zhangpeng <zhangpeng@fenbi.com>
2018-03-15 23:16:48 +0800

    ADD: 营业收入

--- convert.py
+++ convert.py
@@ -12,7 +12,11 @@ def main():
         # ignore header
         # 000001,1999-03,0.007358
         for row in source_file:
-            d1[row[0]][row[1].split('-')[0]] = row[2]
+            a = d1[row[0]][row[1].split('-')[0]]
+            i_a = float(a) if a else -1000
+            i_b = float(row[2]) if row[2] else -1000
+            if i_a < i_b or not a:
+                d1[row[0]][row[1].split('-')[0]] = row[2]
 
     l2 = []
     with open('../data/dest.csv', 'rb') as csvfile_dest:
@@ -22,7 +26,7 @@ def main():
         for row in dest_file:
             if row and row[0]:
                 # 600528, 2001
-                l2.append((row[0]))
+                l2.append((row[0], row[1].strip()))
 
     # d3 = collections.defaultdict(list)
     # for day, key in l2:
@@ -36,14 +40,13 @@ def main():
     #     for j in range(0 if i - 37 < 0 else i - 37, i):
     #         d3[(day, key)].append((d1[key][j][0], d1[key][j][1]))
 
-    with open('../data/result4.csv', 'wb') as f:
+    with open('../data/result.csv', 'wb') as f:
         writer = csv.writer(f, delimiter=',')
         for key in l2:
             try:
-                for v in sorted(d1[key]):
-                    writer.writerow([key, v, d1[key][v]])
+                writer.writerow([key[0], key[1], d1[key[0].zfill(6)][key[1]]])
             except:
-                writer.writerow([key, '', ''])
+                writer.writerow([key[0], key[1], ''])
 
 
 if __name__ == '__main__':
